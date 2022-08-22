542d78484e10006e1dfbf88700ced7e6f4b6c11b
dalia saud <daliasaud5@gmail.com>
2022-08-22 12:33:07 +0300

    First Commit

---
+++ localization.js
@@ -0,0 +1,754 @@
+module.exports = {
+
+//   **HOMEPAGE**  5 - 445
+    //navigation bar
+    languageNav:{
+        en: "العربية",
+        ar: "English"
+    },
+    featuersNav:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    blogNav:{
+        en: "Blog",
+        ar: "المدونة"
+    },
+    featuresNavMenu:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    //from right to left
+    autoCatogrised1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    autoCatogrisedDisc1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    autoCatogrised2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    autoCatogrisedDisc2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    autoCatogrised3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    autoCatogrisedDisc3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    autoCatogrised4:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    autoCatogrisedDisc4:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    autoCatogrised5:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    autoCatogrisedDisc5:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    autoCatogrised6:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    autoCatogrisedDisc6:{
+        en: "lorem",
+        ar: "lorem"
+    },
+        //
+
+
+        //
+    //hero section
+    heroTitle:{
+        en: "lorem",
+        ar: "اتخذ قرارات مالية أفضل مع ملاءة"
+    },
+    heroDisc:{
+        en: "lorem",
+        ar: "إدارة الأموال والمصاريف وتتبعها أو حتى الاستثمار أمور قد تكون معقدة. ملاءة تقدم لك حل للتبع جميع أمورك المالية من عدة حسابات بنكية في مكان واحد. تتبع مصاريفك، راقب مستواك الائتماني واستثمر مدخراتك"
+    },
+        //
+      
+
+        //
+    downloadButton:{
+        en: "lorem",
+        ar: "حمل تطبيق ملاءة "
+    },
+        //
+
+
+        //
+    youCanSave:{
+        en: "lorem",
+        ar: "يمكِّنكم الإدخار وإدارة المصاريف من الوصول للحرية المالية وتحقيق الأهداف التالية"
+    },
+    //from right to left
+    youCanSaveInvest:{
+        en: "lorem",
+        ar: "الاستثمار"
+    },
+    youCanSaveAssets:{
+        en: "lorem",
+        ar: "تملك الأصول"
+    },
+    youCanSaveEmergency:{
+        en: "lorem",
+        ar: "تخصيص مبلغ للطوارئ"
+    },
+    youCanSaveUrSon:{
+        en: "lorem",
+        ar: "مصاريف الأبناء"
+    },
+
+    // 2 more youCanSave in phone ver.
+
+        //
+    
+
+         //
+    companysLogo:{
+        en: "lorem",
+        ar: "lorem"
+    },
+        //
+
+
+        //
+    newWay:{
+        en: "lorem",
+        ar: "الطريقة الجديدة"
+    },
+    newWayDiscP1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    newWayDiscP2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    newWayDiscP3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    newWayDiscP4:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    oldWay:{
+        en: "lorem",
+        ar: "الطريقة التقليدية"
+    },
+    oldWayDiscP1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    oldWayDiscP2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    oldWayDiscP3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    oldWayDiscP4:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    
+    pfmInMalaa:{
+        en: "lorem",
+        ar: "إدارة المصاريف في ملاءة"
+    },
+        //
+
+
+        //
+    //products
+
+    //Financial Management
+    productFM:{
+        en: "lorem",
+        ar: "إدارة المصاريف"
+    },
+    productFMTitle:{
+        en: "lorem",
+        ar: "ادخار أفضل، ادخار أسهل"
+    },
+    //from top to bottom
+    productFMTitle1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productFMDisc1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productFMTitle2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productFMDisc2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productFMTitle3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productFMDisc3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    
+    //Investment
+    productInvest:{
+        en: "lorem",
+        ar: "الاستثمار"
+    },
+    productInvestTitle:{
+        en: "lorem",
+        ar: "طريقة سهلة لاستثمار أموالك"
+    },
+    //from top to bottom
+    productInvestTitle1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productInvestDisc1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productInvestTitle2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productInvestDisc2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productInvestTitle3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productInvestDisc3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    //Lending
+    productLending:{
+        en: "lorem",
+        ar: "التمويل"
+    },
+    productLendingTitle:{
+        en: "lorem",
+        ar: "قرارات تمويلية أفضل"
+    },
+    //from top to bottom
+    productLendingTitle1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productLendingDisc1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productLendingTitle2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productLendingDisc2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productLendingTitle3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    productLendingeDisc3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+       //
+
+
+       //
+    privacyTitle:{
+        en: "lorem",
+        ar: "نأخذ الخصوصية بمحمل الجد"
+    },
+    privacyDisc:{
+        en: "lorem",
+        ar: "أولويتنا الأولى هي خصوصيتك وكسب ثقتك. لا نطلع في ملاءة على اسم المستخدم أو كلمة المرور الخاصة بحسابك المصرفي، بل نبرم اتفاقية مع البنك من خلال ترابط تقني آمن. بالإضافة الى التزامنا بالكثير من اللوائح المصرفية"
+    },
+    //from top to bottom
+    privacyBulletList1:{
+        en: "lorem",
+        ar: "بنية تحتية من الطراز الرفيع"
+    },
+    privacyBulletList2:{
+        en: "lorem",
+        ar: "حفظ البيانات مع تشفير 256 بت"
+    },
+    privacyBulletList3:{
+        en: "lorem",
+        ar: "أمان عالي بنفس مستويات البنك"
+    },
+    AuthorizedBy:{
+        en: "lorem",
+        ar: "نحن مصرحين من: "
+    },
+       //
+
+     
+       //
+    saidAboutMalaa:{
+        en: "lorem",
+        ar: "قـالوا عن ملاءة"
+    },
+    saidName:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    saidPosition:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    saidDisc:{
+        en: "lorem",
+        ar: "lorem"
+    },
+
+    // there is more than one said
+
+       //
+
+
+       //
+    // blank container
+       //
+
+
+       //
+   //blog part
+    latestTopics:{
+        en: "lorem",
+        ar: "آخر المواضيع"
+    },
+    //from right to left
+    topic1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    topic2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    topic3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    readMore:{
+        en:"lorem",
+        ar: "اقرأ المزيد"
+    },
+       //
+
+
+       //
+    //footer section
+    company:{
+        en: "lorem",
+        ar: "الشركة"
+    },
+    aboutMalaa:{
+        en: "lorem",
+        ar: "عن ملاءة"
+    },
+    privacyPolicy:{
+        en: "lorem",
+        ar: "سياسة الخصوصية"
+    },
+    termsCondition:{
+        en: "lorem",
+        ar: "شروط الاستخدام"
+    },
+    contactHelp:{
+        en: "lorem",
+        ar: "التواصل والمساعدة"
+    },
+    faq:{
+        en: "lorem",
+        ar: "الأسئلة الشائعة"
+    },
+    hiring:{
+        en: "lorem",
+        ar: "التوظيف"
+    },
+    downloadMalaa:{
+        en: "lorem",
+        ar: "حمل تطبيق ملاءة"
+    },
+    downloadMalaaIOS:{
+        en: "Download for iOS",
+        ar: "Download for iOS"
+    },
+    downloadMalaaAndroid:{
+        en: "Download for Android",
+        ar: "Download for Android"
+    },
+   malaaRights:{
+    en: "lorem",
+    ar: "© جميع الحقوق محفوظة لشركة ملاءة"
+   },
+        //
+
+
+        //
+    fotterP1:{
+        en: "lorem",
+        ar: 'إنّ شركة "ملاءة" مسجلة في مركز دبي المالي العالمي ("DIFC")، وتخضع لرقابة سلطة دبي للخدمات المالية ("DFSA")، وتهدف إلى إجراء خدمات ماليّة مثل "ترتيب الصفقات في الاستثمارات" بمصادقة "التجزئة". ولا تقدّم شركة "رصيد" أي مشورة تجارية أو استثمارية، وبالتالي فإنها ليست مسؤولةً عن أي خسارة ناجمة عن أي استثمار بالاعتماد على أي معلومات عامّة، سواء كانت مقدمة أو متاحة، على موقعها الإلكتروني والخدمات الأخرى المستندة إلى الويب (يُشار إليها إجمالًا باسم "خدمات الموقع الإلكتروني"). ولا تدّعي شركة "رصيد" أن معلوماتها دقيقة أو موثوقة أو كاملة أو أن التزويد بها سيكون بدون انقطاع. ولا تعكس أي معلومات متعلّقة بطرف ثالث تم تقديمها من خلال "رصيد"، آراء الشركة أبدًا.'
+       },
+    fotterP2:{
+        en: "lorem",
+        ar: 'ويهدف محتوى خدمات الموقع الإلكتروني الذي تقدمه "رصيد" إلى تزويدك بمعلومات عامة فقط، فهو ليس عرض بيع أو التماسًا لعرض شراء أي ورقة مالية، وبالتالي لا يجوز الاعتماد عليه لأغراض الاستثمار. وكذلك لا ينبغي اعتبار أي تعليقات، أو مقالات، أو أخبار يومية، أو منشورات الدردشة العامة و/ أو الخاصة، أو تحليل الأسهم و/ أو أي معلومات أخرى واردة في خدمات الموقع، كنصيحة استثمارية.'
+       },
+    fotterP3:{
+        en: "lorem",
+        ar: 'ولن تكون شركة "رصيد" مسؤولةً عن أي تأخير أو عدم دقة أو خطأ أو إغفال في أي نوع من المعلومات التي تقدّمها، و/أو يقدّمها مزود معلومات تابع لجهة خارجية. وكذلك لن تتحمّل الشركة مسؤولية أي خسارة أو ضرر قد تتعرض له نتيجة اعتماد المعلومات التي تقدمها الشركة، و/أو أي مزود معلومات تابع لجهة خارجية. وبالإضافة إلى ذلك، لا تتحمل الشركة أي مسؤولية عن أي خسائر ناجمة عن الوصول غير المصرح به إلى المعلومات أو أي سوء استخدام آخر لها..'
+       },
+    fotterP4:{
+        en: "lorem",
+        ar:'وبالمثل، فإنّ أي آراء أو أخبار أو أبحاث أو تحليلات أو أسعار أو غيرها من المعلومات الواردة في خدمات الموقع الإلكتروني الخاصة بنا، أو التي يتم إرسالها إليك عبر البريد الإلكتروني هي بمثابة تعليق عام على السوق، ولا تُعتبر نصيحة استثمارية. وبناءً عليه، لن تتحمل الشركة مسؤولية أي خسارة أو ضرر، بما في ذلك، على سبيل المثال لا الحصر، أي خسارة في الربح قد تنشأ بشكل مباشر أو غير مباشر عن استخدام هذه المعلومات أو الاعتماد عليها. وإنّ كل قرار تتخذه بشأن إذا ما كان الاستثمار مناسبًا أو سليمًا، هو قرار مستقل تابع لك. فأنت توافق على أن "رصيد" ليس لديها واجب ائتماني تجاهك، وليست مسؤولة عن أي التزامات ومطالبات وأضرار وتكاليف ونفقات، بما في ذلك أتعاب المحاماة الناجمة عن اعتمادك على معلومات الاستثمار العامة التي تقدّمها الشركة. ولا تقدّم شركة "رصيد" أي إقرارات في إذا ما كان استثمار معين مناسبًا أو سليمًا بالنسبة لك.'
+       },
+        //
+
+
+
+        
+//    **TERMS&CONDITIONS PAGE**   452 - 593
+
+          //
+    termsConditionTitle:{
+    en: "lorem",
+    ar: "الأحكام والشروط"
+    },
+          //
+           
+          //
+    termsConditionIntro:{
+        en: "lorem",
+        ar: "مقدمة أحكام الأحكام"
+    },
+    termsConditionIntroP1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    termsConditionIntroP2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+          //
+
+          //
+    otherTerms:{
+        en: "lorem",
+        ar: "شروط أخرى قابلة للتطبيق"
+    },
+    otherTermsP1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    otherTermsP2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+         //
+
+         //
+    changeTerms:{
+        en: "lorem",
+        ar: "التغييرات على هذه الشروط"
+    },
+    changeTermsP1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    changeTermsP2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+         //
+
+         //
+    changeSite:{
+        en: "lorem",
+        ar: "التغييرات على موقعنا"
+    },
+    changeSiteP1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    changeSiteP2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+          //
+
+          //
+    accountPassword:{
+        en: "lorem",
+        ar: "حسابك وكلمة المرور"
+    },
+    accountPasswordP1:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    accountPasswordP2:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    accountPasswordP3:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    accountPasswordP4:{
+        en: "lorem",
+        ar: "lorem"
+    },
+          //
+
+          //
+   authentication:{
+    en: "lorem",
+    ar: "المصادقة"
+   },
+   authenticationP1:{
+    en: "lorem",
+    ar: "lorem"
+   },
+   authenticationP2:{
+    en: "lorem",
+    ar: "lorem"
+   },
+        //
+        
+        //
+   accessSite:{
+    en: "lorem",
+    ar: "الوصول إلى موقعنا"
+   },
+   accessSiteP1:{
+    en: "lorem",
+    ar: "lorem"
+   },
+   accessSiteP2:{
+    en: "lorem",
+    ar: "lorem"
+   },
+   accessSiteP3:{
+    en: "lorem",
+    ar: "lorem"
+   },
+   accessSiteP4:{
+    en: "lorem",
+    ar: "lorem"
+   },
+        //
+
+        //
+   propertyRights:{
+    en: "lorem",
+    ar: "حقوق الملكية الفكرية"
+   },
+   propertyRightsP1:{
+    en: "lorem",
+    ar: "lorem"
+   },
+   propertyRightsP2:{
+    en: "lorem",
+    ar: "lorem"
+   },
+       //
+
+
+
+
+//      **FEATURES PAGE**  600 - 613
+
+          //
+
+    //hero section+discreption
+
+    investment:{
+     en: "lorem",
+     ar: "lorem"
+    },
+    investmentDisc:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    // four more headlines
+         //
+
+
+
+
+//     **ABOUT US PAGE**   620 - 686
+
+           //
+    ourMission:{
+        en: "lorem",
+        ar: "مهمتنا"
+    },
+    ourMissionIsToSupport:{
+        en: "lorem",
+        ar: "تكمن مهمتنا في دعم اتخاذ قراراتك المالية"
+    },
+    ourMissionIsToSupportDisc:{
+        en: "lorem",
+        ar: "ملاءة هي مجموعة من المنتجات المالية التقنية والتي تساهم في الاستراتيجية الوطنية للادخار بالاضافة لمساعدة الأشخاص على:"
+    },
+    ourMissionIsToSupportP1:{
+        en: "lorem",
+        ar: "التخطيط لأمورهم المالية على المدى الطويل"
+    },
+    ourMissionIsToSupportP2:{
+        en: "lorem",
+        ar: "التقليل من النفقات الزائدة ومساعدتهم في تبني اسلوب حياة مالي مستدام"
+    },
+    ourMissionIsToSupportP3:{
+        en: "lorem",
+        ar: "زيادة الوعي المالي والأخذ بزمام أمورهم المالية"
+    },
+    ourMissionIsToSupportP4:{
+        en: "lorem",
+        ar: "القدرة على اتخاذ القرارات المالية بشكل صحيح"
+    },
+             //
+
+    //hero section titel+discription
+
+             //
+    founders:{
+        en: "lorem",
+        ar: "المؤسسين"
+    },
+    ali:{
+        en: "lorem",
+        ar: "علي العريني"
+    },
+    aliPosition:{
+        en: "lorem",
+        ar: "مؤسس، الرئيس التنفيذي"
+    },
+    aliDisc:{
+        en: "lorem",
+        ar: "lorem"
+    },
+    faisal:{
+        en: "lorem",
+        ar: "فيصل القرني"
+    },
+    faisalPosition:{
+        en: "lorem",
+        ar: "مؤسس، رئيس الاستراتيجية"
+    },
+    faisalDisc:{
+        en: "lorem",
+        ar: "lorem"
+    },
+            //
+        
+    // companys logo
+     
+            //
+
+
+
+
+//     **PARTNEERSHIP PAGE**   693-751
+
+            //
+    partnershipTitle:{
+        en: "lorem",
+        ar: "الشراكة طريق ذي اتجاهين"
+    },
+
+    // from right to left
+    partnershipTitle1:{
+        en: "lorem",
+        ar: "معدلات تفاعل أعلى"
+    },
+    partnershipDisc1:{
+        en: "lorem",
+        ar: "قدم أدوات أفضل لإدارة الأموال، وزد من تفاعل المستخدمين لديك، واحصل على تفاعل أعلى، واكتشف مصادر دخل جديدة."
+    },
+    partnershipTitle2:{
+        en: "lorem",
+        ar: "قاعدة عملاء أكبر"
+    },
+    partnershipDisc2:{
+        en: "lorem",
+        ar: "اكتشف السلوك المالي لعملائك وزد من قيمتهم الدائمة من خلال تجربة مصرفية أفضل."
+    },
+    partnershipTitle3:{
+        en: "lorem",
+        ar: "تكاليف عمليات أقل"
+    },
+    partnershipDisc3:{
+        en: "lorem",
+        ar: "قم بالوصول إلى بيانات دقيقة ومعمقة، وقم ببناء عمليات بيع أكثر كفاءة وتوفير التكاليف على المبيعات والعمليات."
+    },
+    partnershipTitle4:{
+        en: "lorem",
+        ar: "معلومات عن تفضيلات مستخدميك"
+    },
+    partnershipDisc4:{
+        en: "lorem",
+        ar: "تعرف بالضبط على ما يفضله عملاؤك، وقم بتطوير منتجات مصرفية أفضل ، وعزز تجربة العملاء الشاملة ، وحدد أساليب استهداف فعالة. "
+    },
+           //
+
+           //
+    ourPartners:{
+        en: "lorem",
+        ar: "شركاؤنا"
+    },
+    banks:{
+        en: "lorem",
+        ar: "البنوك"
+    },
+    fundingCopmanies:{
+        en: "lorem",
+        ar: "شركات التمويل"
+    },
+    techCopmanies:{
+        en: "lorem",
+        ar: "شركات التقنية"
+    },
+         //
+
+
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("#btn")
+
+button.addEventListener("click",function(){
+    document.getElementById('container').style.justifyContent = "justify-end";
+})
